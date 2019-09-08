a50142fc1576af750b2f22368a75bb240abcfa0e
RytisDr <r.drazdauskas@gmail.com>
2019-09-08 19:11:25 +0200

    Initial commit, some files for agent part.

---
+++ agent/agent-signup-form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <form id="agentForm" action="api-signup" method="POST">
+        <input type="email" placeholder="Email" name="txtEmail" id="inputEmail" value="a@a.com">
+        <input type="password" placeholder="Password" name="txtAgentPass1" id="">
+        <input type="password" placeholder="Repeat the Password" name="txtAgentPass2" id="">
+        <button>Sign Up</button>
+    </form>
+</body>
+
+</html>
